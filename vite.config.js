c0041e7c5c90b9a0240623dadb43f3a2cccd1636
Dhruv <officialdhruv0004@gmail.com>
2024-11-24 18:32:46 +0530

    fix: update vite config for react-type-animation and rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
+  optimizeDeps: {
+    include: ['react-type-animation']
+  },
   build: {
     rollupOptions: {
-      external: ['react-type-animation']
+      output: {
+        manualChunks: {
+          'react-type': ['react-type-animation']
+        }
+      }
     }
   }
 })
